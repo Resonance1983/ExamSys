c7f052d4e298f6bdf625f8e984ecb4b479f28d43
zgyqq <89819496+zgyqq@users.noreply.github.com>
2022-05-17 22:29:13 +0800

    Add files via upload
    项目初始化

---
+++ ExamSys/src/test/java/com/example/ExamSys/ExamSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExamSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
